f409a6c3218672d89c56872ce3337957c8de6a7f
Dhia Mbarki <tahridhia22@gmail.com>
2021-03-29 19:28:43 +0100

    add fireBase

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>let's build chat app</h1>
+    <div className="app">
+      <h1>let's build chat app</h1>
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
     </div>
   );
 }
