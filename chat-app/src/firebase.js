e5d0de7bdc956ee3ccd15d6b378a26a91323bd65
Dhia Mbarki <tahridhia22@gmail.com>
2021-03-29 19:08:50 +0100

    create react app

---
+++ chat-app/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB6ivWtr2-Q3tpyFkboDMoOh_VC0ra5XRA",
+  authDomain: "chat-app-34cd9.firebaseapp.com",
+  projectId: "chat-app-34cd9",
+  storageBucket: "chat-app-34cd9.appspot.com",
+  messagingSenderId: "783328097677",
+  appId: "1:783328097677:web:14d0252b3c96f6658df9c4",
+};
