866a1bff3fd5089ccf1c0c824578828b570189c4
Dhia Mbarki <tahridhia22@gmail.com>
2021-03-30 16:25:35 +0100

    IconButton

--- chat-app/src/sidebar/Sidebar.js
+++ chat-app/src/sidebar/Sidebar.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import './sidebar.css'
-import {Avatar} from "@material-ui/core"
-import DonutLargeIcon from "@material-ui/icons/DonutLarge"
-import ChatIcon from "@material-ui/icons/Chat"
-import MoreVertIcon from "@material-ui/icons/MoreVert"
-
+import React from "react";
+import "./sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="sidebar__header">
-                    <Avatar/>
-                    <div className="sidebar__headerRight">
-                        <DonutLargeIcon/>
-                        <ChatIcon/>
-                        <MoreVertIcon/>
-                    </div>
-            </div>
-            <div className = "sidebar__search">
-
-            </div>
-
-        <div className="sidebar__chats"></div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+          <MoreVertIcon />
+          </IconButton>
         </div>
-    )
+      </div>
+      <div className="sidebar__search"></div>
+
+      <div className="sidebar__chats"></div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
