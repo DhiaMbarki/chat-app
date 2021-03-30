0368474e8c3fbea6237c5b972ea526fbf720bf81
Dhia Mbarki <tahridhia22@gmail.com>
2021-03-30 19:19:08 +0100

    chat side

---
+++ chat-app/src/chats/ChatSide.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './chat.css'
+function ChatSide() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ChatSide
--- chat-app/src/sidebar/Sidebar.js
+++ chat-app/src/sidebar/Sidebar.js
@@ -1,30 +1,36 @@
 import React from "react";
 import "./sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
-import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from "@material-ui/icons/Chat";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
+import {SearchOutlined} from "@material-ui/icons";
+import ChatSide from "../chats/ChatSide";
+
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__header">
+      <div className="sidebar_header">
         <Avatar />
-        <div className="sidebar__headerRight">
-          <IconButton>
-            <DonutLargeIcon />
-          </IconButton>
-          <IconButton>
-            <ChatIcon />
-          </IconButton>
-          <IconButton>
-          <MoreVertIcon />
-          </IconButton>
-        </div>
+      
       </div>
-      <div className="sidebar__search"></div>
+      <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input type="text" placeholder="Search"/>
+                </div>
+            </div>
 
-      <div className="sidebar__chats"></div>
+      <div className="sidebar_chats">
+          <ChatSide/>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+        <h1>Chat</h1>
+
+      </div>
     </div>
   );
 }
