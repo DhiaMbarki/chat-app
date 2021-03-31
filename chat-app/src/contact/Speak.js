2d694d105b7f2f8428170ae2b5b50c6ea60f3458
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-01 00:18:19 +0100

    section 2 formBody/Done

--- chat-app/src/contact/Speak.js
+++ chat-app/src/contact/Speak.js
@@ -1,50 +1,47 @@
-import React, {useEffect, useState} from 'react';
-import "./speak.css"
-import { Avatar,IconButton} from "@material-ui/core";
-import { Message, MoreVert, SearchOutlined} from '@material-ui/icons';
-import VideoCallIcon from '@material-ui/icons/VideoCall';
-import CallIcon from '@material-ui/icons/Call';
+import React, { useEffect, useState } from "react";
+import "./speak.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { Message, MoreVert, SearchOutlined } from "@material-ui/icons";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import CallIcon from "@material-ui/icons/Call";
 function Speak() {
-    const [seed, setSeed] = useState("");
-    
-   
-    useEffect(() => {
-        setSeed(Math.floor(Math.random() * 5000));        
-    }, []);
+  const [seed, setSeed] = useState("");
 
-    return (
-        <div className="chat">
-            <div className="chat_header">
-                
-            <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
-            <div className="chat_headerInfo">
-                <h3>Romm name ⬇️</h3>
-                <p>Last view at ...</p>
-                </div>
-            
-            <div className="chat_headerRight">
-                    <IconButton>
-                    <VideoCallIcon/>
-                    </IconButton>
-                   
-                    <IconButton>
-                        <CallIcon/>
-                    </IconButton>
-                    
-                </div>
-            </div>
-            <div className="chat_body">
-<p className="chat_message">
-<span className="chat_name">Dhia Mbarki</span>
-Yo Yo i m here
-<span className="chat_timestamp">
-3:52pm 
-</span>
-</p>
-            </div>
-            <div className="chat_footer"></div>
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat_header">
+        <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`} />
+        <div className="chat_headerInfo">
+          <h3>Romm name ⬇️</h3>
+          <p>Last view at ...</p>
         </div>
-    )
+
+        <div className="chat_headerRight">
+          <IconButton>
+            <VideoCallIcon />
+          </IconButton>
+
+          <IconButton>
+            <CallIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className='chat_body'>
+                
+                    <p className={`chat_message ${ true && 'chat_receiver'}`}>
+                        <span className="chat_name">Dhia Mbarki</span>
+                        yala yala I m here
+                        <span className="chat_timestemp">5:40pm</span>
+                    </p>
+                
+            </div>
+      <div className="chat_footer"></div>
+    </div>
+  );
 }
 
-export default Speak
+export default Speak;
