613a7980329b29b72b4c6c4b32f2ab6770ed5fdb
Dhia Mbarki <tahridhia22@gmail.com>
2021-04-01 16:41:27 +0100

    firebase google auth

--- chat-app/src/firebase.js
+++ chat-app/src/firebase.js
@@ -1,3 +1,6 @@
+import { Db } from "@material-ui/icons";
+import firebase from "firebase"
+
 const firebaseConfig = {
   apiKey: "AIzaSyB6ivWtr2-Q3tpyFkboDMoOh_VC0ra5XRA",
   authDomain: "chat-app-34cd9.firebaseapp.com",
@@ -6,3 +9,12 @@ const firebaseConfig = {
   messagingSenderId: "783328097677",
   appId: "1:783328097677:web:14d0252b3c96f6658df9c4",
 };
+
+
+const firebaseApp = firebase.initializeApp (firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export default Db;
